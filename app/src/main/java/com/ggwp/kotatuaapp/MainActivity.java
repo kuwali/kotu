05701f2c3e38d04414dbf3b0bc3fb4831e4ed158
akmaljun <akmaljuniawan@gmail.com>
2016-06-19 20:32:10 +0700

    Update 19 Juni 2016
    Tambah Main Activity yang telah dimodifikasi tampilannya

--- app/src/main/java/com/ggwp/kotatuaapp/MainActivity.java
+++ app/src/main/java/com/ggwp/kotatuaapp/MainActivity.java
@@ -2,33 +2,146 @@ package com.ggwp.kotatuaapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
+import com.github.aakira.expandablelayout.Utils;
+import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationItem;
+import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationView;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+import com.synnapps.carouselview.ViewListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MainActivity extends Activity {
+
+    CarouselView customCarouselView;
+    int NUMBER_OF_PAGES = 4;
+
+    int[] sampleImages = {R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher};
+    String[] sampleTitles = {"Orange", "Grapes", "Strawberry", "Cherry", "Apricot"};
+    String[] sampleNetworkImageURLs = {
+            "https://placeholdit.imgix.net/~text?txtsize=15&txt=image1&txt=350%C3%97150&w=350&h=150",
+            "https://placeholdit.imgix.net/~text?txtsize=15&txt=image2&txt=350%C3%97150&w=350&h=150",
+            "https://placeholdit.imgix.net/~text?txtsize=15&txt=image3&txt=350%C3%97150&w=350&h=150",
+            "https://placeholdit.imgix.net/~text?txtsize=15&txt=image4&txt=350%C3%97150&w=350&h=150",
+            "https://placeholdit.imgix.net/~text?txtsize=15&txt=image5&txt=350%C3%97150&w=350&h=150"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView helloWorld = (TextView) findViewById(R.id.hello);
-        helloWorld.setText("Hello guys");
-        final EditText inputText = (EditText) findViewById(R.id.inputEditText);
+//        TextView helloWorld = (TextView) findViewById(R.id.hello);
+//        helloWorld.setText("Hello guys");
+//        final EditText inputText = (EditText) findViewById(R.id.inputEditText);
+//
+//        Button submitButton = (Button) findViewById(R.id.submitButton);
+//        submitButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String ambilText = inputText.getText().toString();
+//                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+//                intent.putExtra("Ganteng", ambilText);
+//                startActivity(intent);
+//            }
+//        });
 
-        Button submitButton = (Button) findViewById(R.id.submitButton);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String ambilText = inputText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-                intent.putExtra("Ganteng",ambilText);
-                startActivity(intent);
-            }
-        });
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+
+        BottomNavigationItem bottomNavigationItem = new BottomNavigationItem
+                ("Record", ContextCompat.getColor(this, R.color.firstColor), R.mipmap.ic_launcher);
+        BottomNavigationItem bottomNavigationItem1 = new BottomNavigationItem
+                ("Like", ContextCompat.getColor(this, R.color.secondColor), R.mipmap.ic_launcher);
+        bottomNavigationView.addTab(bottomNavigationItem);
+        bottomNavigationView.addTab(bottomNavigationItem1);
+
+        customCarouselView = (CarouselView) findViewById(R.id.carouselView);
+        customCarouselView.setPageCount(NUMBER_OF_PAGES);
+        // set ViewListener for custom view
+        customCarouselView.setViewListener(viewListener);
+
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        final List<ItemModel> data = new ArrayList<>();
+        data.add(new ItemModel(
+                "0 ACCELERATE_DECELERATE_INTERPOLATOR",
+                R.color.material_red_500,
+                R.color.material_red_300,
+                Utils.createInterpolator(Utils.ACCELERATE_DECELERATE_INTERPOLATOR)));
+        data.add(new ItemModel(
+                "1 ACCELERATE_INTERPOLATOR",
+                R.color.material_pink_500,
+                R.color.material_pink_300,
+                Utils.createInterpolator(Utils.ACCELERATE_INTERPOLATOR)));
+        data.add(new ItemModel(
+                "2 BOUNCE_INTERPOLATOR",
+                R.color.material_purple_500,
+                R.color.material_purple_300,
+                Utils.createInterpolator(Utils.BOUNCE_INTERPOLATOR)));
+        data.add(new ItemModel(
+                "3 DECELERATE_INTERPOLATOR",
+                R.color.material_deep_purple_500,
+                R.color.material_deep_purple_300,
+                Utils.createInterpolator(Utils.DECELERATE_INTERPOLATOR)));
+        data.add(new ItemModel(
+                "4 FAST_OUT_LINEAR_IN_INTERPOLATOR",
+                R.color.material_indigo_500,
+                R.color.material_indigo_300,
+                Utils.createInterpolator(Utils.FAST_OUT_LINEAR_IN_INTERPOLATOR)));
+        data.add(new ItemModel(
+                "5 FAST_OUT_SLOW_IN_INTERPOLATOR",
+                R.color.material_blue_500,
+                R.color.material_blue_300,
+                Utils.createInterpolator(Utils.FAST_OUT_SLOW_IN_INTERPOLATOR)));
+        data.add(new ItemModel(
+                "6 LINEAR_INTERPOLATOR",
+                R.color.material_light_blue_500,
+                R.color.material_light_blue_300,
+                Utils.createInterpolator(Utils.LINEAR_INTERPOLATOR)));
+        data.add(new ItemModel(
+                "7 LINEAR_OUT_SLOW_IN_INTERPOLATOR",
+                R.color.material_cyan_500,
+                R.color.material_cyan_300,
+                Utils.createInterpolator(Utils.LINEAR_OUT_SLOW_IN_INTERPOLATOR)));
+        recyclerView.setAdapter(new RecyclerViewRecyclerAdapter(data));
     }
+
+    ViewListener viewListener = new ViewListener() {
+
+        @Override
+        public View setViewForPosition(int position) {
+            View customView = getLayoutInflater().inflate(R.layout.view_custom, null);
+            //set view attributes here
+            TextView labelTextView = (TextView) customView.findViewById(R.id.labelTextView);
+            ImageView fruitImageView = (ImageView) customView.findViewById(R.id.fruitImageView);
+
+
+            fruitImageView.setImageResource(sampleImages[position]);
+            labelTextView.setText(sampleTitles[position]);
+
+            //customView.setIndicatorGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP);
+
+            return customView;
+        }
+
+
+    };
 }
